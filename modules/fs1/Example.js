f9abe502e9039ae0692b57822701d266fb19bde1
USER 1 <gchkumar999@gmail.com>
2024-04-19 10:50:55 +0530

    prat

---
+++ modules/fs1/Example.js
@@ -0,0 +1,30 @@
+// // const fs = require('fs')
+
+// // Synchronous
+// // let susthir = fs.readFileSync('data.txt','utf-8')
+
+// // console.log(susthir);
+
+// import fs from 'fs'
+
+// //Asynchronous
+// let data =fs.readFile('data.txt','utf-8',(err,data)=>{
+//     if(err) throw err
+//     console.log(data);
+// })
+
+
+// const fs =require('fs')
+
+// //Synchronous
+// let data =fs.readFileSync('data.txt','utf-8')
+// console.log(data);
+
+
+import fs from 'fs'
+
+//ASynchronous
+let data=fs.readFile('data.txt','utf-8',(err,data)=>{
+if (err)throw err
+console.log(data);
+})
