fc04492a3bd3c9f70c3a0192e48340a2457377a3
chaitanyakumar99 <gchkumar999@gmail.com>
2024-12-22 11:28:57 +0530

    txt

--- NodeJS_Nov/NodeJS Basics/FS-JSON-2 Filter male and female _emp/readandwritefile.js
+++ NodeJS_Nov/NodeJS Basics/FS-JSON-2 Filter male and female _emp/readandwritefile.js
@@ -2,22 +2,30 @@ import fs from 'fs'
 
 // store male and female emp in seperate file
 
-fs.readFile('data.json','utf-8',(err,data)=>{
+fs.readFile('data.json', 'utf-8', (err, data) => {
     if (err) throw err
-    let emps=JSON.parse(data)
+    let emps = JSON.parse(data)
     console.log(data)
 
-let male_emp=[]
-let female_emp=[]
+    let male_emp = []
+    let female_emp = []
 
-for(emp of emps){
-    if (user.gender==='Male') {
-        male_emp.push(emp)
+    for (let emp of emps) {
+        if (emp.gender === 'Male') {
+            male_emp.push(emp)
+        }
+        if(emp.gender ==='Female'){
+            female_emp.push(emp)
+        }
+
     }
 
-}
-
-    fs.writeFile('male.json',data,(err)=>{
-
+    fs.writeFile('male.json', JSON.stringify(male_emp), (err) => {
+        if (err) throw err
+        console.log("New json file is created")
+    })
+    fs.writeFile('feamale.json',JSON.stringify(female_emp),(err)=>{
+        if(err) throw err
+        console.log("New Female file is created")
     })
 })
