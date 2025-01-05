e929f5662017c64636c4405a1bd0eab7398383ee
chaitanyakumar99 <gchkumar999@gmail.com>
2025-01-05 23:58:09 +0530

    txt

--- NodeJS_Nov/ExpressJS-All/Express-Basics/app.js
+++ NodeJS_Nov/ExpressJS-All/Express-Basics/app.js
@@ -1,7 +1,7 @@
 import exprss from 'express'
 import path from 'path'   
 let app = exprss()
-//http://127.0.0.1:8080/
+//http://127.0.0.1:8081/
 app.get("/",(req,resp)=>{
     resp.send("Root Req")
 })
