f337b7315533977884d1111fd37121f9440eaeef
chaitanyakumar99 <gchkumar999@gmail.com>
2025-01-05 21:29:55 +0530

    txt

--- NodeJS_Nov/User_Jwt/model/User.js
+++ NodeJS_Nov/User_Jwt/model/User.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose'
 
 let userShema = mongoose.Schema({
+    
     user_Name: { type: String, require: true },
     email: { type: String, require: true },
     mobile: { type: Number, require: true },
