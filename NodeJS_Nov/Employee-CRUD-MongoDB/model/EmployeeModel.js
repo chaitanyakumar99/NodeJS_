5c4e94fd674913347e22920511f3937c9f10db37
chaitanyakumar99 <gchkumar999@gmail.com>
2025-01-16 16:41:25 +0530

    txt

--- NodeJS_Nov/Employee-CRUD-MongoDB/model/EmployeeModel.js
+++ NodeJS_Nov/Employee-CRUD-MongoDB/model/EmployeeModel.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 let empSchema = mongoose.Schema({
+    
     eid: { type: Number, require: true },
     ename: { type: String, require: true },
     esal: { type: Number, require: true },
